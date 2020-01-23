1d790cd4c2a12b0ee28cd45c18516d18ad6ae7d2
Yacinesaf <yacinesaf96@gmail.com>
2020-01-23 17:00:47 -0500

    work is progressing !

--- src/Medium.js
+++ src/Medium.js
@@ -4,6 +4,14 @@ import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import PinterestIcon from '@material-ui/icons/Pinterest';
 import BigImg from './alex-knight-5-GNa303REg-unsplash.jpg'
+import img1 from './artiom-vallat-1uBCLmu5BqA-unsplash.jpg'
+import img2 from './artiom-vallat-awHbsDUY5FI-unsplash.jpg'
+import img3 from './aviv-rachmadian-5fZqHF21CIw-unsplash.jpg'
+import img4 from './balkouras-nicos-BMBUEXaFvGI-unsplash.jpg'
+import img5 from './emmanuel-WcrqKjgMPfI-unsplash.jpg'
+
+
+
 function Medium() {
   const space = (type, direction, value) => {
     let types = { m: 'margin', p: 'padding' }
@@ -28,6 +36,15 @@ function Medium() {
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
   const xsOnly = useMediaQuery(theme.breakpoints.only('xs'));
+  let imagesList = [img1, img2, img3, img4, img5];
+  let timeList = ['23 min read', '75 min read', '15 min read', '9 min read', '45 min read'];
+  let titleList = [
+    'How Mormons Built the Next Silicon Valley While No One Was Looking',
+    'We Finally Figured Out Who Makes wikiHow’s Bizarre Art',
+    'How Science Fiction Imagined the 2020s',
+    'How to Configure Your iPhone to Work for You, Not Against You',
+    'Survival of the Richest',
+  ];
 
 
 
@@ -38,10 +55,10 @@ function Medium() {
           <AppBar position='static' style={{ backgroundColor: 'white', boxShadow: '0 4px 12px 0 rgba(0, 0, 0, 0.05)' }} elevation={0}>
             <Toolbar style={smDown ? space('p', 'x', 20) : space('p', 'x', 150)}>
               <PinterestIcon fontSize='large' style={{ display: smDown ? 'unset' : 'none', color: 'black' }} />
-              <Typography style={{ color: 'black', fontWeight: 600, display: smDown ? 'none' : 'unset' }} variant='h5'>
+              <Typography style={{ color: 'black', fontWeight: 600, display: smDown ? 'none' : 'unset', paddingRight: 10 }} variant='h5'>
                 Medium
               </Typography>
-              <Typography style={{ color: 'black', flexGrow: 1, paddingLeft: 10 }} variant='subtitle1'>
+              <Typography style={{ color: 'black', flexGrow: 1, paddingLeft: 10, borderLeft: 'solid 1px lightgrey' }} variant='subtitle1'>
                 Technology
               </Typography>
               <div style={{ display: smDown ? 'none' : 'flex' }}>
@@ -52,17 +69,57 @@ function Medium() {
                   Sign in
               </Typography>
               </div>
-              <Button color='inherit' variant='outlined' style={{ color: 'rgba(2, 158, 116, 1)', fontSize: xsOnly ? 10 : 'unset' }}>
+              <Button color='inherit' variant='outlined' style={{ color: 'rgba(2, 158, 116, 1)', textTransform: 'none' }} size='small'>
                 Get Started
               </Button>
             </Toolbar>
           </AppBar>
         </Grid>
       </Grid>
-      <Grid container justify='center'>
-        <Grid item lg={8} xl={8} md={10} sm={10} xs={12} >
-          <div style={{height : 400, width : 600,  backgroundImage: `url(${BigImg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <Grid container>
+        <Grid item lg={8} xl={8} md={9} sm={12} xs={12} style={{ paddingLeft: smDown ? 0 : 150, paddingTop: 60, paddingRight: 60 }}>
+          <div style={{ height: xsOnly ? 300 : 450, width: '100%', backgroundImage: `url(${BigImg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
           </div>
+        </Grid>
+        <Grid item lg={4} xl={4} md={3} sm={12} xs={12} style={{ paddingLeft: 30, paddingRight: 150 }}>
+          <Typography variant='h4' style={Object.assign({}, space('p', 't', 60), { fontWeight: 500 })}>
+            Technology
+          </Typography>
+          <Typography variant='subtitle1' style={{ paddingTop: 10, color: 'grey' }}>
+            The download.
+          </Typography>
+          <Button size="medium" variant='outlined' style={{ marginTop: 20, textTransform: 'none' }} color='inherit'>
+            Follow
+          </Button>
+          <Typography style={{ marginTop: 25, color: 'grey' }}>
+            Follow to get great stories about Technology in your inbox and on your homepage
+          </Typography>
+          <Typography variant="subtitle1" color='inherit' style={{ fontWeight: 600, marginTop: 40, borderBottom: '1px solid lightgrey' }}>
+            RELATED TOPICS
+          </Typography>
+          <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10, paddingTop: 20 }} >SOFTWARE ENGENEERING</Typography>
+          <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >PROGRAMMING</Typography>
+          <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >ARTIFICIAL INTELLIGENCE</Typography>
+          <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >CRYPTOCURRENCY</Typography>
+          <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >BIOTECH</Typography>
+          <Typography variant="subtitle1" color='inherit' style={{ fontWeight: 600, marginTop: 40, borderBottom: '1px solid lightgrey' }}>
+            POPULAR IN TECHNOLOGY
+          </Typography>
+          {imagesList.map((x, i) => (
+            <div style={{ display: 'flex' }}>
+              <div>
+                <Typography color='inherit' style={{ paddingBottom: 10, paddingTop: 20, fontWeight: 600 }} >
+                  {titleList[i]}
+                </Typography>
+                <Typography variant='caption' style={{ display: 'block' }}>
+                  {timeList[i]}
+                </Typography>
+              </div>
+              <div
+                style={{ maxHeight: 60, minWidth: 60, maxWidth: 60, minHeight: 60, backgroundImage: `url(${x})`, backgroundSize: 'cover', backgroundPosition: 'center', marginTop: 20, marginLeft: 80 }}>
+              </div>
+            </div>
+          ))}
         </Grid>
       </Grid>
     </div>
