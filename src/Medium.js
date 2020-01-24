84b0920bcce8989773952d3258c428fabf86a4a1
Yacinesaf <yacinesaf96@gmail.com>
2020-01-24 18:32:25 -0500

    boo

--- src/Medium.js
+++ src/Medium.js
@@ -11,7 +11,7 @@ import img4 from './balkouras-nicos-BMBUEXaFvGI-unsplash.jpg'
 import img5 from './emmanuel-WcrqKjgMPfI-unsplash.jpg'
 import sliker from './sliker-main.jpg'
 import StarIcon from '@material-ui/icons/Star';
-
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
 
 function Medium() {
   const space = (type, direction, value) => {
@@ -46,6 +46,26 @@ function Medium() {
     'How to Configure Your iPhone to Work for You, Not Against You',
     'Survival of the Richest',
   ];
+  let bigTitles = [
+    'Software Ate the World. Now It’s Design’s Turn',
+    'Do yourself a favor in 2020: Mute Elon Musk',
+    'What Shoshana Zuboff & Mark Zuckerberg both get wrong about privacy rights, and how you can fix it.'
+  ];
+  let subtitle = [
+    'We are entering a Golden Age of product design, in which even niche enterprise products are finally becoming sensible, easy to use, and…',
+    'Bad tweets from famous people aren’t worth your time.',
+    'Ten practical perspectives that will help you understand how people do (and will) balance privacy in their daily lives.'
+  ];
+  let captions = [
+    'Fast Company in Fast Company',
+    'Mashable',
+    'Jason T. Voiovich'
+  ];
+  let dateTime = [
+    'Jan 23 · 4 min read',
+    'Jan 3 · 10 min read',
+    'Jan 13 · 21 min read'
+  ];
 
 
 
@@ -73,73 +93,97 @@ function Medium() {
               </Button>
         </Toolbar>
       </AppBar>
-      <Grid container>
-        <Grid item lg={8} xl={8} md={9} sm={12} xs={12} style={{ paddingLeft: smDown ? 0 : 150, paddingTop: smDown ? 0 : 60, paddingRight: smDown ? 0 : 60 }}>
-          <div style={{ height: xsOnly ? 300 : 450, width: '100%', backgroundImage: `url(${BigImg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          </div>
-          <Typography variant='h3' style={{ paddingTop: 15, fontWeight: 600 }}>
-            The Future Doesn't Last
-          </Typography>
-          <Typography variant='body1' style={{ color: 'grey', paddingTop: 15 }}>
-            A controversy around old Sonos devices is a reminder that software updates can be a death sentence
-          </Typography>
-          <div style={{ display: 'flex', marginTop: 20 }}>
-            <Avatar alt='sliker' src={sliker} style={{ height: 50, width: 50, boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)' }} />
-            <div style={{ paddingLeft: 10 }}>
-              <Typography variant='body1'>Mohammed Jihad Sliker</Typography>
-              <div style={{display : 'flex'}}>
-              <Typography variant='body2' style={{ color: 'grey' }}>Jan 23 3min read </Typography>
-              <StarIcon style={{fontSize : 15, color : 'grey', paddingLeft : 5, paddingTop : 2}}/>
+      <Grid container justify='center'>
+        <Grid item xl={10} lg={9} md={10} sm={11} xs={11}>
+          <Grid container>
+            <Grid item sm={8} xs={12} style={{ paddingTop: smDown ? 0 : 60, paddingRight : 40 }}>
+              <div style={{ height: xsOnly ? 300 : 400, width: '100%', backgroundImage: `url(${BigImg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
               </div>
-              
-              </div>
-            
-          </div>
-          <Typography variant="subtitle2" style={{fontWeight : 550, borderBottom : '1px solid lightgrey', marginTop : 40}}>LATEST</Typography>
-
-        </Grid>
-        <Grid item lg={4} xl={4} md={3} sm={12} xs={12} style={{ paddingLeft: 30, paddingRight: 150 }}>
-          <Typography variant='h4' style={Object.assign({}, space('p', 't', 60), { fontWeight: 500 })}>
-            Technology
+              <Typography variant='h3' style={{ paddingTop: 15, fontWeight: 600 }}>
+                The Future Doesn't Last
           </Typography>
-          <Typography variant='subtitle1' style={{ paddingTop: 10, color: 'grey' }}>
-            The download.
+              <Typography variant='body1' style={{ color: 'grey', paddingTop: 15 }}>
+                A controversy around old Sonos devices is a reminder that software updates can be a death sentence
           </Typography>
-          <Button size="medium" variant='outlined' style={{ marginTop: 20, textTransform: 'none' }} color='inherit'>
-            Follow
-          </Button>
-          <Typography style={{ marginTop: 25, color: 'grey' }}>
-            Follow to get great stories about Technology in your inbox and on your homepage
-          </Typography>
-          <Typography variant="subtitle1" color='inherit' style={{ fontWeight: 600, marginTop: 40, borderBottom: '1px solid lightgrey' }}>
-            RELATED TOPICS
-          </Typography>
-          <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10, paddingTop: 20 }} >SOFTWARE ENGENEERING</Typography>
-          <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >PROGRAMMING</Typography>
-          <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >ARTIFICIAL INTELLIGENCE</Typography>
-          <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >CRYPTOCURRENCY</Typography>
-          <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >BIOTECH</Typography>
-          <Typography variant="subtitle1" color='inherit' style={{ fontWeight: 600, marginTop: 40, borderBottom: '1px solid lightgrey' }}>
-            POPULAR IN TECHNOLOGY
-          </Typography>
-          <div style={{ paddingBottom: 10 }}>
-            {imagesList.map((x, i) => (
-              <Grid key={i} container justify='space-around'>
-                <Grid item md={9}>
-                  <Typography color='inherit' style={{ paddingTop: 20, fontWeight: 600 }} >
-                    {titleList[i]}
-                  </Typography>
-                  <Typography variant='caption'>
-                    {timeList[i]}
-                  </Typography>
-                </Grid>
-                <Grid item md={3} style={{ paddingLeft: 'calc(25% - 60px)' }}>
-                  <div
-                    style={{ width: 60, height: 60, backgroundImage: `url(${x})`, backgroundSize: 'cover', backgroundPosition: 'center', marginTop: 20 }}>
+              <div style={{ display: 'flex', marginTop: 20 }}>
+                <Avatar alt='sliker' src={sliker} style={{ height: 50, width: 50, boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)' }} />
+                <div style={{ paddingLeft: 10 }}>
+                  <Typography variant='body1'>Mohammed Jihad Sliker</Typography>
+                  <div style={{ display: 'flex' }}>
+                    <Typography variant='body2' style={{ color: 'grey' }}>Jan 23 · 3min read </Typography>
+                    <StarIcon style={{ fontSize: 15, color: 'grey', paddingLeft: 5, paddingTop: 2 }} />
                   </div>
-                </Grid>
-              </Grid>))}
-          </div>
+                </div>
+              </div>
+              <Typography variant="subtitle2" style={{ fontWeight: 550, borderBottom: '1px solid lightgrey', marginTop: 40 }}>LATEST</Typography>
+              <div style={{ paddingBottom: 15 }}>
+                {bigTitles.map((x, i) => (
+                  <Grid container key={i} style={{ paddingTop: 20 }}>
+                    <Grid item xs={10} md={9} style={{ paddingRight: 10 }}>
+                      <Typography style={{ fontWeight: 550, fontSize: 18 }}>{x}</Typography>
+                      <Typography variant='body2' style={{ color: 'grey', paddingBottom: 10, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{subtitle[i]}</Typography>
+                      <Typography variant='body2' style={{ paddingBottom: 5 }}>{captions[i]}</Typography>
+                      <Grid item style={{ display: 'flex' }}>
+                        <div style={{ display: 'flex', flexGrow: 1 }}>
+                          <Typography variant='body2'>{dateTime[i]} </Typography>
+                          <StarIcon style={{ fontSize: 15, color: 'grey', paddingLeft: 5, paddingTop: 2 }} />
+                        </div>
+                        <BookmarkBorderIcon style={{ fontSize: 20 }} />
+                      </Grid>
+                    </Grid>
+                    <Grid item xs={2} md={3} style={{ paddingLeft: 'calc(25% - 140px)' }}>
+                      <div
+                        style={{ width: "100%", height: '100%', backgroundImage: `url(${imagesList[i]})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                      </div>
+                    </Grid>
+                  </Grid>
+                ))}
+              </div>
+            </Grid>
+            <Grid item lg={4} xl={4} md={4} sm={12} xs={12} style={{ paddingLeft: 30, paddingRight: 150 }}>
+            <Typography variant='h4' style={Object.assign({}, space('p', 't', 60), { fontWeight: 500 })}>
+              Technology
+            </Typography>
+            <Typography variant='subtitle1' style={{ paddingTop: 10, color: 'grey' }}>
+              The download.
+            </Typography>
+            <Button size="medium" variant='outlined' style={{ marginTop: 20, textTransform: 'none' }} color='inherit'>
+              Follow
+            </Button>
+            <Typography style={{ marginTop: 25, color: 'grey' }}>
+              Follow to get great stories about Technology in your inbox and on your homepage
+            </Typography>
+            <Typography variant="subtitle1" color='inherit' style={{ fontWeight: 600, marginTop: 40, borderBottom: '1px solid lightgrey' }}>
+              RELATED TOPICS
+            </Typography>
+            <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10, paddingTop: 20 }} >SOFTWARE ENGENEERING</Typography>
+            <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >PROGRAMMING</Typography>
+            <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >ARTIFICIAL INTELLIGENCE</Typography>
+            <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >CRYPTOCURRENCY</Typography>
+            <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >BIOTECH</Typography>
+            <Typography variant="subtitle1" color='inherit' style={{ fontWeight: 600, marginTop: 40, borderBottom: '1px solid lightgrey' }}>
+              POPULAR IN TECHNOLOGY
+            </Typography>
+            <div style={{ paddingBottom: 10, display: smDown ? 'none' : 'unset' }}>
+              {imagesList.map((x, i) => (
+                <Grid key={i} container style={{ paddingTop: 20 }}>
+                  <Grid item sm={9} style={{ paddingRight: 10 }}>
+                    <Typography color='inherit' style={{ fontWeight: 600 }} >
+                      {titleList[i]}
+                    </Typography>
+                    <Typography variant='caption'>
+                      {timeList[i]}
+                    </Typography>
+                  </Grid>
+                  <Grid item sm={3} style={{ paddingLeft: 'calc(25% - 60px)' }}>
+                    <div
+                      style={{ width: 60, height: 60, backgroundImage: `url(${x})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                    </div>
+                  </Grid>
+                </Grid>))}
+            </div>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </div>
