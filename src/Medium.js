38de6df85a40974111a15e653c6a7de52c4cbffa
Yacinesaf <yacinesaf96@gmail.com>
2020-01-25 17:26:34 -0500

    version 1 is over

--- src/Medium.js
+++ src/Medium.js
@@ -71,32 +71,39 @@ function Medium() {
 
   return (
     <div>
+
       <AppBar position='static' style={{ backgroundColor: 'white', boxShadow: '0 4px 12px 0 rgba(0, 0, 0, 0.05)' }} elevation={0}>
-        <Toolbar style={smDown ? space('p', 'x', 20) : space('p', 'x', 150)}>
-          <CloudIcon fontSize='large' style={{ display: smDown ? 'unset' : 'none', color: 'black', paddingRight: 10 }} />
-          <Typography style={{ color: 'black', fontWeight: 600, display: smDown ? 'none' : 'unset', paddingRight: 10 }} variant='h5'>
-            Medium
-              </Typography>
-          <Typography style={{ color: 'black', flexGrow: 1, paddingLeft: 10, borderLeft: 'solid 1px lightgrey' }} variant='subtitle1'>
-            Technology
-              </Typography>
-          <div style={{ display: smDown ? 'none' : 'flex' }}>
-            <Typography style={{ color: 'grey' }}>
-              Become a memeber
-              </Typography>
-            <Typography style={Object.assign({}, { color: 'rgba(2, 158, 116, 1)' }, space('m', 'x', 15))}>
-              Sign in
-              </Typography>
-          </div>
-          <Button color='inherit' variant='outlined' style={{ color: 'rgba(2, 158, 116, 1)', textTransform: 'none' }} size='small'>
-            Get Started
-              </Button>
-        </Toolbar>
+        <Grid container justify='center'>
+          <Grid item xs={11} lg={10} md={11}>
+            <Toolbar style={{ padding: 0 }}>
+              <CloudIcon fontSize='large' style={{ display: smDown ? 'unset' : 'none', color: 'black', paddingRight: 10 }} />
+              <Typography style={{ color: 'black', fontWeight: 600, display: smDown ? 'none' : 'unset', paddingRight: 10 }} variant='h5'>
+                Medium
+            </Typography>
+              <Typography style={{ color: 'black', flexGrow: 1, paddingLeft: 10, borderLeft: 'solid 1px lightgrey' }} variant='subtitle1'>
+                Technology
+            </Typography>
+              <div style={{ display: smDown ? 'none' : 'flex' }}>
+                <Typography style={{ color: 'grey' }}>
+                  Become a memeber
+            </Typography>
+                <Typography style={Object.assign({}, { color: 'rgba(2, 158, 116, 1)' }, space('m', 'x', 15))}>
+                  Sign in
+            </Typography>
+              </div>
+              <Button color='inherit' variant='outlined' style={{ color: 'rgba(2, 158, 116, 1)', textTransform: 'none' }} size='medium'>
+                Get Started
+            </Button>
+            </Toolbar>
+          </Grid>
+        </Grid>
       </AppBar>
+
+
       <Grid container justify='center'>
-        <Grid item xl={10} lg={9} md={10} sm={11} xs={11}>
-          <Grid container>
-            <Grid item sm={8} xs={12} style={{ paddingTop: smDown ? 0 : 60, paddingRight : 40 }}>
+        <Grid className="mainGrid" item xl={10} lg={10} md={11} sm={12} xs={12} style={{ paddingTop: xsOnly ? 0 : 40 }} >
+          <Grid container justify='center'>
+            <Grid item md={8} sm={11} xs={11} style={{ paddingRight: smDown ? 0 : 40 }}>
               <div style={{ height: xsOnly ? 300 : 400, width: '100%', backgroundImage: `url(${BigImg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
               </div>
               <Typography variant='h3' style={{ paddingTop: 15, fontWeight: 600 }}>
@@ -115,73 +122,87 @@ function Medium() {
                   </div>
                 </div>
               </div>
-              <Typography variant="subtitle2" style={{ fontWeight: 550, borderBottom: '1px solid lightgrey', marginTop: 40 }}>LATEST</Typography>
+              <Typography variant="subtitle2" style={{ fontWeight: 550, borderBottom: '1px solid lightgrey', marginTop: 30 }}>LATEST</Typography>
               <div style={{ paddingBottom: 15 }}>
                 {bigTitles.map((x, i) => (
                   <Grid container key={i} style={{ paddingTop: 20 }}>
-                    <Grid item xs={10} md={9} style={{ paddingRight: 10 }}>
-                      <Typography style={{ fontWeight: 550, fontSize: 18 }}>{x}</Typography>
-                      <Typography variant='body2' style={{ color: 'grey', paddingBottom: 10, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{subtitle[i]}</Typography>
-                      <Typography variant='body2' style={{ paddingBottom: 5 }}>{captions[i]}</Typography>
-                      <Grid item style={{ display: 'flex' }}>
-                        <div style={{ display: 'flex', flexGrow: 1 }}>
-                          <Typography variant='body2'>{dateTime[i]} </Typography>
-                          <StarIcon style={{ fontSize: 15, color: 'grey', paddingLeft: 5, paddingTop: 2 }} />
+                    <Grid item xs={10} sm={10} md={9} style={{ paddingRight: 40 }}>
+                      <Typography style={{ fontWeight: 550, fontSize: xsOnly ? 14 : 18 }}>{x}</Typography>
+                      <Typography variant='body2' style={{ color: 'grey', paddingBottom: xsOnly ? 5 : 10, overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}>{subtitle[i]}</Typography>
+                      <div style={{ display: xsOnly ? 'none' : 'unset' }}>
+                        <Typography variant={xsOnly ? 'caption' : 'body2'} style={{ paddingBottom: 5 }}>{captions[i]}</Typography>
+                        <div style={{ display: 'flex' }}>
+                          <div style={{ display: 'flex', flexGrow: 1 }}>
+                            <Typography variant={xsOnly ? 'caption' : 'body2'}>{dateTime[i]} </Typography>
+                            <StarIcon style={{ fontSize: 15, color: 'grey', paddingLeft: 5, paddingTop: 2 }} />
+                          </div>
+                          <BookmarkBorderIcon style={{fontSize: 20 }} />
                         </div>
-                        <BookmarkBorderIcon style={{ fontSize: 20 }} />
-                      </Grid>
+                      </div>
                     </Grid>
-                    <Grid item xs={2} md={3} style={{ paddingLeft: 'calc(25% - 140px)' }}>
+                    <Grid item xs={2} sm={2} md={3}>
                       <div
-                        style={{ width: "100%", height: '100%', backgroundImage: `url(${imagesList[i]})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                        style={{ width: xsOnly ? 56 : "100%", height: xsOnly ? 56 : '100%', backgroundImage: `url(${imagesList[i]})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                      </div>
+                    </Grid>
+                    <Grid item xs={12} style={{ display: xsOnly ? 'unset' : 'none' }}>
+                      <div>
+                        <Typography variant={xsOnly ? 'caption' : 'body2'} style={{ paddingBottom: 5 }}>{captions[i]}</Typography>
+                        <div style={{ display: 'flex' }}>
+                          <div style={{ display: 'flex', flexGrow: 1 }}>
+                            <Typography variant={xsOnly ? 'caption' : 'body2'}>{dateTime[i]} </Typography>
+                            <StarIcon style={{ fontSize: 15, color: 'grey', paddingLeft: 5, paddingTop: 2 }} />
+                          </div>
+                          <BookmarkBorderIcon style={{fontSize: 20 }} />
+                        </div>
                       </div>
                     </Grid>
                   </Grid>
                 ))}
               </div>
             </Grid>
-            <Grid item lg={4} xl={4} md={4} sm={12} xs={12} style={{ paddingLeft: 30, paddingRight: 150 }}>
-            <Typography variant='h4' style={Object.assign({}, space('p', 't', 60), { fontWeight: 500 })}>
-              Technology
+            <Grid item lg={4} xl={4} md={4} sm={12} xs={12} style={{ paddingLeft: 30 }}>
+              <Typography variant='h4' style={{ fontWeight: 500 }}>
+                Technology
             </Typography>
-            <Typography variant='subtitle1' style={{ paddingTop: 10, color: 'grey' }}>
-              The download.
+              <Typography variant='subtitle1' style={{ paddingTop: 10, color: 'grey' }}>
+                The download.
             </Typography>
-            <Button size="medium" variant='outlined' style={{ marginTop: 20, textTransform: 'none' }} color='inherit'>
-              Follow
+              <Button size="medium" variant='outlined' style={{ marginTop: 20, textTransform: 'none' }} color='inherit'>
+                Follow
             </Button>
-            <Typography style={{ marginTop: 25, color: 'grey' }}>
-              Follow to get great stories about Technology in your inbox and on your homepage
+              <Typography style={{ marginTop: 25, color: 'grey' }}>
+                Follow to get great stories about Technology in your inbox and on your homepage
             </Typography>
-            <Typography variant="subtitle1" color='inherit' style={{ fontWeight: 600, marginTop: 40, borderBottom: '1px solid lightgrey' }}>
-              RELATED TOPICS
+              <Typography variant="subtitle1" color='inherit' style={{ fontWeight: 600, marginTop: 40, borderBottom: '1px solid lightgrey' }}>
+                RELATED TOPICS
             </Typography>
-            <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10, paddingTop: 20 }} >SOFTWARE ENGENEERING</Typography>
-            <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >PROGRAMMING</Typography>
-            <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >ARTIFICIAL INTELLIGENCE</Typography>
-            <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >CRYPTOCURRENCY</Typography>
-            <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >BIOTECH</Typography>
-            <Typography variant="subtitle1" color='inherit' style={{ fontWeight: 600, marginTop: 40, borderBottom: '1px solid lightgrey' }}>
-              POPULAR IN TECHNOLOGY
+              <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10, paddingTop: 20 }} >SOFTWARE ENGENEERING</Typography>
+              <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >PROGRAMMING</Typography>
+              <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >ARTIFICIAL INTELLIGENCE</Typography>
+              <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >CRYPTOCURRENCY</Typography>
+              <Typography style={{ color: 'grey', fontSize: 12, paddingBottom: 10 }} >BIOTECH</Typography>
+              <Typography variant="subtitle1" color='inherit' style={{ fontWeight: 600, marginTop: 40, borderBottom: '1px solid lightgrey' }}>
+                POPULAR IN TECHNOLOGY
             </Typography>
-            <div style={{ paddingBottom: 10, display: smDown ? 'none' : 'unset' }}>
-              {imagesList.map((x, i) => (
-                <Grid key={i} container style={{ paddingTop: 20 }}>
-                  <Grid item sm={9} style={{ paddingRight: 10 }}>
-                    <Typography color='inherit' style={{ fontWeight: 600 }} >
-                      {titleList[i]}
-                    </Typography>
-                    <Typography variant='caption'>
-                      {timeList[i]}
-                    </Typography>
-                  </Grid>
-                  <Grid item sm={3} style={{ paddingLeft: 'calc(25% - 60px)' }}>
-                    <div
-                      style={{ width: 60, height: 60, backgroundImage: `url(${x})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                    </div>
-                  </Grid>
-                </Grid>))}
-            </div>
+              <div style={{ paddingBottom: 10, display: smDown ? 'none' : 'unset' }}>
+                {imagesList.map((x, i) => (
+                  <Grid key={i} container style={{ paddingTop: 20 }}>
+                    <Grid item sm={9} style={{ paddingRight: 10 }}>
+                      <Typography color='inherit' style={{ fontWeight: 600 }} >
+                        {titleList[i]}
+                      </Typography>
+                      <Typography variant='caption'>
+                        {timeList[i]}
+                      </Typography>
+                    </Grid>
+                    <Grid item sm={3} style={{ paddingLeft: 'calc(25% - 60px)' }}>
+                      <div
+                        style={{ width: 60, height: 60, backgroundImage: `url(${x})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+                      </div>
+                    </Grid>
+                  </Grid>))}
+              </div>
             </Grid>
           </Grid>
         </Grid>
